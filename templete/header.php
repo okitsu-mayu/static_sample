e83ca0c790977fc9be1b8c2dac898b71459c129c
okitsu-mayu <okitsumayu@gmail.com>
2023-05-19 23:49:58 +0900

    ヘッダーリンク修正

--- templete/header.php
+++ templete/header.php
@@ -6,12 +6,12 @@
     <ul class="menu">
         <?php if($lang == "en"){ ?>
             <!-- 英語版メニュー -->
-            <li class="menu-list"><a href="/">TOP</a></li>
+            <li class="menu-list"><a href="/static_sample">TOP</a></li>
             <li class="menu-list"><a href="sample1">sample1</a></li>
             <li class="menu-list"><Button type="button" onclick="setLanguage('ja')">日本語</Button></li>
         <?php }else{ ?>
             <!-- 日本語版メニュー -->
-            <li class="menu-list"><a href="index">トップ</a></li>
+            <li class="menu-list"><a href="/static_sample">トップ</a></li>
             <li class="menu-list"><a href="sample1">サンプル1</a></li>
             <li class="menu-list"><a href="sample2">サンプル2</a></li>
             <li class="menu-list"><Button type="button" onclick="setLanguage('en')">English</Button></li>
